7a544778ea53c5e4d5c1e60405117c4c6c18eac6
kpencak <katarzyna.pencak23@gmail.com>
2019-10-21 19:26:44 +0200

    Added zad2 and txt file for zad1

--- AHP-GMM/zad2.py
+++ AHP-GMM/zad2.py
@@ -47,18 +47,42 @@ def geo_index(matrix):
     n = numpy.size(matrix, 1)
     vector = gmm(matrix)
     sum = 0
-    for i in range(n):
+    for i in range(1, n):
         for j in range(i+1, n):
             sum += math.pow(math.log10(matrix[i][j]*(vector[j]/vector[i])), 2)
     IG = 2/((n-1)*(n-2))*sum
     return IG
 
 
+def koczkodaj(matrix):
+    n = numpy.size(matrix, 1)
+    vector = []
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                product = (matrix[i][k]*matrix[k][j])/matrix[i][j]
+                minimum = min(numpy.abs(1-product), numpy.abs(1-(1/product)))
+                vector.append(minimum)
+    result = max(vector)
+    return result
+    
 indexA = satty_index(A)
 indexB = satty_index(B)
 indexC = satty_index(C)
-indextest = satty_index(test)
+print("Indeks Satty'ego dla macierzy A: ", indexA, "\n",
+      "Indeks Satty'ego dla macierzy B: ", indexB, "\n",
+      "Indeks Satty'ego dla macierzy C: ", indexC)
 
-#index_geo_A = geo_index(A)
-index_geo_test = geo_index(test)
-print(index_geo_test)
+index_geo_A = geo_index(A)
+index_geo_B = geo_index(B)
+index_geo_C = geo_index(C)
+print("Indeks geometryczny dla macierzy A: ", index_geo_A, "\n",
+      "Indeks geometryczny dla macierzy B: ", index_geo_B, "\n",
+      "Indeks geometryczny dla macierzy C: ", index_geo_C)
+
+index_kocz_A = koczkodaj(A)
+index_kocz_B = koczkodaj(B)
+index_kocz_C = koczkodaj(C)
+print("Indeks Koczkodaja dla macierzy A: ", index_kocz_A, "\n",
+      "Indeks Koczkodaja dla macierzy B: ", index_kocz_B, "\n",
+      "Indeks Koczkodaja dla macierzy C: ", index_kocz_C)
